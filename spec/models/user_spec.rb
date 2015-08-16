f24a1efd8966ec0595469ed63af9408f484432db
Takamatsu Makoto <tkmtmkt@gmail.com>
2015-08-16 22:46:45 +0900

    6.3.1 暗号化されたパスワード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe User, type: :model do
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
 
   it { should be_valid }
 
