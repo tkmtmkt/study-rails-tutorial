92e6b8cbc8c9422a0247c5a833e264720606a172
Takamatsu Makoto <tkmtmkt@gmail.com>
2015-08-16 22:13:11 +0900

    6.2.1 最初のユーザーテスト  リスト6.5

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = User.new(name: "Example User", email: "user@example.com") }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
 end
