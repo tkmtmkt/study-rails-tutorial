c6b968922764db137f8380b904709e2a472a1c2e
Takamatsu Makoto <tkmtmkt@gmail.com>
2015-08-16 18:02:54 +0900

    5.3.2 Railsのルート  リスト5.24

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
