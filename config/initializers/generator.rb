6b15e6c18e403429ec42317da23cd25030157900
Takamatsu Makoto <tkmtmkt@gmail.com>
2015-08-29 13:04:03 +0900

    generator初期設定追加

---
+++ config/initializers/generator.rb
@@ -0,0 +1,8 @@
+Rails.application.config.generators do |g|
+  g.stylesheets false
+  g.javascripts false
+  g.helper false
+  g.template_engine :slim
+  g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
+  g.fixture_replacement :factory_girl, dir: "spec/support/factories"
+end
